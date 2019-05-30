8ac3ee663c5f8ac1678ce0bc278539790e981368
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2019-05-30 17:14:50 +0100

    Level 11 - completed

--- lib/Docking_Station_class.rb
+++ lib/Docking_Station_class.rb
@@ -1,7 +1,19 @@
 require '/Users/acfj/projects/Boris_bikes/lib/bike.rb'
 
+
+
 class DockingStation
+attr_reader :docked_bike
+
+  def initialize
+    @docked_bike = []
+  end
+
+  def dock(bike)
+    @docked_bike << bike
+  end
+
   def release_bike
-    bike = Bike.new
+  Bike.new
   end
 end
--- spec/describe_dockingstation_spec.rb
+++ spec/describe_dockingstation_spec.rb
@@ -1,11 +1,14 @@
 require 'docking_station_class'
 
 describe DockingStation do
-    docking_station = DockingStation.new
+    station = DockingStation.new
   it 'should respond to release_bike' do
-    expect(docking_station).to respond_to(:release_bike)
+    expect(station).to respond_to(:release_bike)
   end
   it 'should return an instance of bike class' do
-    expect(docking_station.release_bike).to be_an_instance_of(Bike)
+    expect(station.release_bike).to be_an_instance_of(Bike)
+  end
+  it 'stores bike instance as an attribute of DockingStation instance' do
+    expect(station.dock(bike)).to be_an_instance_of(Bike)
   end
 end
